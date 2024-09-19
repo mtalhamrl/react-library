34794dab62ce401502bce0c93cab79c640a37686
mtalhamrl <110231439+mtalhamrl@users.noreply.github.com>
2024-09-19 21:02:03 +0300

    heros and library services

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import "./App.css";
 import { Navbar } from "./layouts/NavbarandFooter/Navbar";
+import { ExploreTopBooks } from "./layouts/HomePage/ExploreTopBooks";
+import { Carousel } from "./layouts/HomePage/Carousel";
+import { Heros } from "./layouts/HomePage/Heros";
+import { LibraryServices } from "./layouts/HomePage/LibraryServices";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      <ExploreTopBooks />
+      <Carousel />
+      <Heros />
+      <LibraryServices />
+    </div>
+  );
 }
 
 export default App;
