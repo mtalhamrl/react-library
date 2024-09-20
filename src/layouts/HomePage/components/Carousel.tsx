c3424d52a7aef25af79d860605e12c53b00750ef
mtalhamrl <110231439+mtalhamrl@users.noreply.github.com>
2024-09-20 21:27:44 +0300

    added spinner loading for ux

--- src/layouts/HomePage/components/Carousel.tsx
+++ src/layouts/HomePage/components/Carousel.tsx
@@ -1,6 +1,7 @@
 import { ReturnBook } from "./ReturnBook";
 import { useEffect, useState } from "react";
 import BookModel from "../../../models/BookModel";
+import { SpinnerLoading } from "../../utils/SpinnerLoading";
 export const Carousel = () => {
   const [books, setBooks] = useState<BookModel[]>([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -46,11 +47,7 @@ export const Carousel = () => {
   }, []);
 
   if (isLoading) {
-    return (
-      <div className="container m-5">
-        <p>Loading..</p>
-      </div>
-    );
+    return <SpinnerLoading />;
   }
 
   if (httpError) {
