d986ed38af0b0848bb10b50ca1bb57a3bfb0ae11
mtalhamrl <110231439+mtalhamrl@users.noreply.github.com>
2024-09-20 21:54:27 +0300

    create basit seachbookpage and searchbook

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { HomePage } from "./layouts/HomePage/HomePage";
 import { Footer } from "./layouts/NavbarandFooter/Footer";
 import { Navbar } from "./layouts/NavbarandFooter/Navbar";
+import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
 
 export const App = () => {
   return (
     <div>
       <Navbar />
-      <HomePage />
+      {/*     <HomePage /> */}
+      <SearchBooksPage />
       <Footer />
     </div>
   );
